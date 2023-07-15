535de1f5c617c85eab9acbbdda5e4e33e11c5b68
Léo <69589034+Zomblard@users.noreply.github.com>
2023-07-15 18:31:29 +0200

    [Fix] id / for checkbox

--- Views/settings.admin.view.php
+++ Views/settings.admin.view.php
@@ -29,8 +29,8 @@ $description = LangManager::translate("support.description");
             <div class="card">
                 <div class="card-header">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="captcha" name="support_settings_use_mail" <?= $config->getUseMail() ? 'checked' : '' ?>>
-                        <label class="form-check-label" for="captcha"><h6><?= LangManager::translate("support.settings.mail") ?></h6></label>
+                        <input class="form-check-input" type="checkbox" id="support_settings_use_mail" name="support_settings_use_mail" <?= $config->getUseMail() ? 'checked' : '' ?>>
+                        <label class="form-check-label" for="support_settings_use_mail"><h6><?= LangManager::translate("support.settings.mail") ?></h6></label>
                     </div>
                 </div>
                 <div class="card-body">
@@ -42,8 +42,8 @@ $description = LangManager::translate("support.description");
                     </div>
                     <div>
                         <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" id="captcha" name="support_settings_use_sender_mail" <?= $config->getUseSenderMail() ? 'checked' : '' ?>>
-                            <label class="form-check-label" for="captcha"><h6><?= LangManager::translate("support.settings.senderMail") ?></h6></label>
+                            <input class="form-check-input" type="checkbox" id="support_settings_use_sender_mail" name="support_settings_use_sender_mail" <?= $config->getUseSenderMail() ? 'checked' : '' ?>>
+                            <label class="form-check-label" for="support_settings_use_sender_mail"><h6><?= LangManager::translate("support.settings.senderMail") ?></h6></label>
                         </div>
                         <div class="form-group">
                             <input class="form-control" type="email" name="support_settings_custom_sender_mail" placeholder="noreply@mail.com"
@@ -75,16 +75,16 @@ $description = LangManager::translate("support.description");
                 </div>
                 <div class="card-body">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="captcha" name="support_settings_use_webhook_new_support" <?= $config->getUseWebhookNewSupport() ? 'checked' : '' ?>>
-                        <label class="form-check-label" for="captcha"><h6>Discord Webhook - <?= LangManager::translate("support.settings.newRequest") ?> :</h6></label>
+                        <input class="form-check-input" type="checkbox" id="support_settings_use_webhook_new_support" name="support_settings_use_webhook_new_support" <?= $config->getUseWebhookNewSupport() ? 'checked' : '' ?>>
+                        <label class="form-check-label" for="support_settings_use_webhook_new_support"><h6>Discord Webhook - <?= LangManager::translate("support.settings.newRequest") ?> :</h6></label>
                     </div>
                     <div class="form-group">
                         <input class="form-control" type="url" name="support_settings_webhook_new_support" placeholder="https://discord.com/api/webhooks/" value="<?= $config->getWebhookNewSupport() ?>">
                     </div>
 
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="captcha" name="support_settings_use_webhook_new_response" <?= $config->getUseWebhookNewResponse() ? 'checked' : '' ?>>
-                        <label class="form-check-label" for="captcha"><h6>Discord Webhook - <?= LangManager::translate("support.settings.newResponses") ?> :</h6></label>
+                        <input class="form-check-input" type="checkbox" id="support_settings_use_webhook_new_response" name="support_settings_use_webhook_new_response" <?= $config->getUseWebhookNewResponse() ? 'checked' : '' ?>>
+                        <label class="form-check-label" for="support_settings_use_webhook_new_response"><h6>Discord Webhook - <?= LangManager::translate("support.settings.newResponses") ?> :</h6></label>
                     </div>
                     <div class="form-group">
                         <input class="form-control" type="url" name="support_settings_webhook_new_response" placeholder="https://discord.com/api/webhooks/" value="<?= $config->getWebhookNewResponse() ?>">
@@ -101,10 +101,10 @@ $description = LangManager::translate("support.description");
                 <div class="card-body">
                     <div class="row">
                         <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" id="captcha" name="support_settings_captcha" <?= $config->getCaptcha() ? 'checked' : '' ?>>
-                            <label class="form-check-label" for="captcha"><?= LangManager::translate("support.settings.captcha") ?></label>
+                            <input class="form-check-input" type="checkbox" id="support_settings_captcha" name="support_settings_captcha" <?= $config->getCaptcha() ? 'checked' : '' ?>>
+                            <label class="form-check-label" for="support_settings_captcha"><?= LangManager::translate("support.settings.captcha") ?></label>
                         </div>
-                        <!--TODO : Gérer ceci :-->
+                        <!--TODO : Gérer ceci :
                         <div class="form-check form-switch mt-2">
                             <input class="form-check-input" type="checkbox" id="captcha" name="support_settings_captcha" <?= $config->getCaptcha() ? 'checked' : '' ?>>
                             <label class="form-check-label" for="captcha"><?= LangManager::translate("support.settings.visibility") ?>
@@ -116,7 +116,7 @@ $description = LangManager::translate("support.description");
                                 <option value="0" <?php //$style === $currentStyle ? "selected" : "" ?>><?= LangManager::translate("support.settings.private") ?></option>
                                 <option value="1" <?php //$style === $currentStyle ? "selected" : "" ?>><?= LangManager::translate("support.settings.public") ?></option>
                             </select>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
             </div>
