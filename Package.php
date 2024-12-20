bb4f3ccf691c9ceb9965aa53bb1caeb4e0a02216
Léo <69589034+Zomblard@users.noreply.github.com>
2024-12-20 16:49:44 +0100

    [VERS] Edit version

--- Package.php
+++ Package.php
@@ -16,7 +16,7 @@ class Package implements IPackageConfig
 
     public function version(): string
     {
-        return '1.0.0';
+        return '1.0.1';
     }
 
     public function authors(): array
